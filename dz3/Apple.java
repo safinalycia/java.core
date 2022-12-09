acdfe2d74aa8448823c4c4df98092d1280d8dde9
lycia <safinalycia@mail.ru>
2022-12-09 12:59:53 +0300

    загружено 4 домашнее задание

--- dz3/Apple.java
+++
@@ -1,8 +0,0 @@
-package dz3;
-
-public class Apple extends Fruit{
-
-    public Apple(String nameFruit, Integer weightFruit) {
-        super("Apple", 10);
-    }
-}
--- dz3/Orange.java
+++
@@ -1,8 +0,0 @@
-package dz3;
-
-public class Orange extends Fruit{
-
-    public Orange(String nameFruit, Integer weightFruit) {
-        super("Orange", 15);
-    }
-}
